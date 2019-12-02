56e18c0558ddab30680aba08961d551171e1ebbe
JE2Se <834590049@qq.com>
2019-12-02 21:41:04 +0800

    修复Kali运行异常
    修复Kali运行异常

--- lib/color.py
+++ lib/color.py
@@ -43,3 +43,37 @@ elif "Windows" in platform.system():
         PURPLE= ""  
         BROWN= ""  
         WHITE= "" 
+elif "Linux" in platform.system():
+    class Vcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        RED = '\033[31m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+        YELLOW= '\033[1;33m'
+        DARKGRAY= "\033[1;30m"   
+        CYAN= "\033[0;36m"   
+        PURPLE= "\033[0;35m"  
+        BROWN= "\033[0;33m"  
+        WHITE= "\033[1;37m"
+else:
+    class Vcolors:
+        HEADER = ''
+        OKBLUE = ''
+        OKGREEN = ''
+        WARNING = ''
+        FAIL = ''
+        RED = ''
+        ENDC = ''
+        BOLD = ''
+        UNDERLINE = ''
+        YELLOW= ''
+        DARKGRAY= ""   
+        CYAN= ""   
+        PURPLE= ""  
+        BROWN= ""  
+        WHITE= ""
